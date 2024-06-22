161e38442a736fd7dd38505e2c5a5b26c1f178a5
Albert Janse van Rensburg <4578622+AlbertJvR@users.noreply.github.com>
2024-06-22 19:19:43 +1200

    added routes to api

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,6 +1,7 @@
 import express, { Router, Request, Response } from "express";
 import Cart from "../entities/cart";
 import { collections } from "../services/database.service";
+import { ObjectId } from "mongodb";
 
 const cartRoutes = Router();
 
@@ -27,6 +28,44 @@ cartRoutes.post('/', async (req: Request, res: Response) => {
             : res.status(500).send('Well looks like I still suck at this');   
     } catch (error: any) {
         console.error(error);
+        res.status(400).send(error.message);
+    }
+});
+
+cartRoutes.put('/:id', async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const updatedCart: Cart = req.body as Cart;
+        const query = { _id: new ObjectId(id) };
+
+        const result = await collections.carts?.updateOne(query, { $set: updatedCart });
+
+        result
+            ? res.status(200).send('Updated successfully')
+            : res.status(304).send('Update failed');
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
+
+cartRoutes.delete('/:id', async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = {_id: new ObjectId(id)};
+        const result = await collections.carts?.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully removed`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Cart with id ${id} does not exist`);
+        }
+    } catch (error: any) {
+        console.error(error.message);
         res.status(400).send(error.message);
     }
 });
